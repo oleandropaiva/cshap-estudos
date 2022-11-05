b663e54247b9806ed636d0b15e84593f75f74eb0
Leandro Paiva <leandroapaiva@yahoo.com>
2022-11-05 17:53:41 -0300

    Variáveis Ponto Flutuante

---
+++ project2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace project2
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Projeto 2 - Variáveis Ponto Flutuante");
+            double salario;
+            salario = 3000.10;
+
+            Console.WriteLine(salario);
+        }
+    }
+}
