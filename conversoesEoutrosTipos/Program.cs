99529134a3b75e099cabdcbddd2a6c18a3afdd0c
Leandro Paiva <leandroapaiva@yahoo.com>
2022-11-05 22:24:44 -0300

    trabalhando com inteiros

--- conversoesEoutrosTipos/Program.cs
+++ conversoesEoutrosTipos/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace convrsoesEoutrosTipos
+{
+class Programa
+{
+  static void Main(string[] args)
+  {
+      double salario;
+      salario = 3000.15;
+
+      int salarioInteiro;
+      salarioInteiro = (int)salario;
+
+    Console.WriteLine("Tecle enter para fechar...");
+    Console.ReadLine();
+    }
+  }
+};
