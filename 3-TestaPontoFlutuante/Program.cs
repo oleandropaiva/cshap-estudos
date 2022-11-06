1cae2324448d0996ba6e7c34ee26a2550975c538
Leandro Paiva <leandroapaiva@yahoo.com>
2022-11-05 21:58:14 -0300

    TestaPontoFlutuante

--- 3-TestaPontoFlutuante/Program.cs
+++ 3-TestaPontoFlutuante/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace TestaPontoFlutuante
+{
+class Programa
+{
+  static void Main(string[] args)
+  {
+    Console.WriteLine("Projeto 3 - Variáveis Ponto Flutuante");
+
+    double salario = 3000.10;
+
+    Console.WriteLine("O meu salário é " + salario);
+    }
+  }
+};
