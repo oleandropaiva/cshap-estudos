a5bc531a2efd2f977c82ec7a3c0cf485e6a85338
Leandro Paiva <leandroapaiva@yahoo.com>
2022-11-05 23:16:13 -0300

    três variáveis do tipo double

--- projConversor/Program.cs
+++ projConversor/Program.cs
@@ -6,10 +6,15 @@ namespace projConversor
     {
         static void Main(string[] args)
         {
-            double salario = 1270.50;
+            double salario = 3000.10;
             int valor = (int)salario;
-
             Console.WriteLine(valor);
+
+            double valor1 = 0.2;
+            double valor2 = 0.1;
+            double total = valor1 + valor2;
+
+            Console.WriteLine(total);
         }
     }
 }
