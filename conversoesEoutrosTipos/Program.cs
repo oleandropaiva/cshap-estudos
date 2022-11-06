311ce0f377d378ae3c51ba649998dcaa3a06b999
Leandro Paiva <leandroapaiva@yahoo.com>
2022-11-05 22:45:53 -0300

    new program

--- conversoesEoutrosTipos/Program.cs
+++ conversoesEoutrosTipos/Program.cs
@@ -6,11 +6,13 @@ class Programa
 {
   static void Main(string[] args)
   {
-      double salario;
-      salario = 3000.15;
+      double salario = 3000.15;
 
-      int salarioInteiro;
-      salarioInteiro = (int)salario;
+      int salarioInteiro = (int)salario;
+
+      long x = 2000000000000000000; // 64bits
+      short y = 150000; // 16bits
+      float z = 3.14f; // 32bits
 
     Console.WriteLine("Tecle enter para fechar...");
     Console.ReadLine();
