3b170771474df65056cd53c47029546639021636
Leandro Paiva <leandroapaiva@yahoo.com>
2022-11-06 23:40:34 -0300

    corrige valor

--- caracteresEtextos/Program.cs
+++ caracteresEtextos/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace projConversor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
