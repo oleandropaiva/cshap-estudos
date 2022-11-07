5b6c13d9aa9047eb0b03e3be82b6c8fc0e6f3542
Leandro Paiva <leandroapaiva@yahoo.com>
2022-11-07 15:42:21 -0300

    converte num em caracter com char

--- conversoesEoutrosTipos/Program.cs
+++ conversoesEoutrosTipos/Program.cs
@@ -6,7 +6,12 @@ class Programa
 {
   static void Main(string[] args)
   {
+    Console.WriteLine("Conversões e outros tipos");
+
     char letra = 'a';
+    Console.WriteLine(letra);
+
+    letra = (char)65;
     Console.WriteLine(letra);
 
     Console.WriteLine("Tecle enter para fechar...");
