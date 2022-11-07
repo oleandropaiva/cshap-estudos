e3a59e538324461f1f9dde2d0044e64b4bb725f3
Leandro Paiva <leandroapaiva@yahoo.com>
2022-11-07 16:14:18 -0300

    cria proj

---
+++ atribuicoesDeVariaveis/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace atribuicoesDeVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          Console.WriteLine("Executando o projeto Atribuições de Variáveis.");
+
+          int idade = 32;
+          int idadeGustavo = idade;
+
+          Console.WriteLine(idade);
+          
+        }
+    };
+};
