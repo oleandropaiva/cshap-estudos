27923cf72b59c2cda2530d610f1dcc6235c203fb
Leandro Paiva <leandroapaiva@yahoo.com>
2022-11-07 15:13:08 -0300

    exercício

---
+++ exercicio/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace exercício1
+{
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Exercício aula 1");
+
+        string nome = "Leandro!!";
+
+        Console.WriteLine("Olá, " + nome);
+
+        Console.WriteLine("Tecle enter para fechar...");
+        Console.ReadLine();
+    }
+  }
+};
