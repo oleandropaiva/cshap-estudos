48151ab1f3940594bdc2d885f9aa402d11bcb563
Leandro Paiva <leandroapaiva@yahoo.com>
2022-11-07 17:23:48 -0300

    cria proj condicionais

---
+++ condicionais/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace condicionais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          Console.WriteLine("Executando o projeto Atribuições de Variáveis.");
+
+          int idade = 32;
+          int idadeGustavo = idade;
+
+          Console.WriteLine(idade);          
+        }
+    };
+};
