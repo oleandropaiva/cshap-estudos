f6953eb029194d3c2778aaacba47d530aa34ecef
Leandro Paiva <leandroapaiva@yahoo.com>
2022-11-07 16:04:06 -0300

    quebra de linhas com @

--- conversoesEoutrosTipos/Program.cs
+++ conversoesEoutrosTipos/Program.cs
@@ -17,8 +17,21 @@ class Programa
     letra = (char)(86 + 1);
     Console.WriteLine(letra);
 
-    string primeiraFrase = "Curso de Tecnologia - C#";
+    string primeiraFrase = "Curso de Tecnologia - C# ";
     Console.WriteLine(primeiraFrase + 2022);
+
+    string vazia = "";
+    Console.WriteLine(vazia);
+
+    letra = ' ';
+
+    string cursos = @"Cursos de Tecnologia: 
+    Go
+    C#
+    Java
+    Python";
+    Console.WriteLine(cursos);
+
 
     Console.WriteLine("Tecle enter para fechar...");
     Console.ReadLine();
