e0b3a43994201256cc00afdcbe48deb06635a267
Leandro Paiva <leandroapaiva@yahoo.com>
2022-11-08 15:13:50 -0300

    otimizando

--- condicionais2/Program.cs
+++ condicionais2/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace condicionais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          Console.WriteLine("Executando o projeto Condicionais.");
+
+          int idadeJoão = 16;
+          int quantidadePessoas = 2;
+
+
+          bool acompanhado = quantidadePessoas > 1;
+          bool grupo = true;
+
+          if (idadeJoão >= 18 || grupo)
+          {
+            Console.WriteLine("Pode entrar!");
+          }
+          else
+          {
+              Console.WriteLine("Não pode entrar!");
+          }
+
+          Console.WriteLine("Tecle enter para fechar ...");      
+          Console.ReadLine();    
+        }
+    }
+}
