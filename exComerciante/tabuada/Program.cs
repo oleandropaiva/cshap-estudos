8ae195db11f0cde30c3cbc8292d01c382109ca76
Leandro Paiva <leandroapaiva@yahoo.com>
2022-11-07 23:27:39 -0300

    imprime a tabuada

---
+++ exComerciante/tabuada/Program.cs
@@ -0,0 +1,23 @@
+﻿// Faça um programa que leia um número emostre a tabuada de multiplicação dele
+
+using System;
+
+namespace Tabuada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero, resultado;
+
+            Console.WriteLine("Digite um número e veja a sua tabuada: ");
+            numero = int.Parse(Console.ReadLine());
+
+            for (int contador = 1; contador <= 10; contador++)
+            {
+                resultado = numero * contador;
+                Console.WriteLine(numero + " x " + contador + " = " + resultado);
+            }
+        }
+    }
+}
