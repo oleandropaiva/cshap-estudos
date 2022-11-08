c70de431a18fe1784cba4ae5d2747c7088a75382
Leandro Paiva <leandroapaiva@yahoo.com>
2022-11-08 16:05:55 -0300

    bool

--- escopoVar/Program.cs
+++ escopoVar/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace condicionais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          Console.WriteLine("Executando o projeto Condicionais.");
+
+          int idadeJoão = 16;
+          int quantidadePessoas = 2;
+
+
+          bool acompanhado = quantidadePessoas > 1;
+
+          string textoAdicional;
+
+          if(acompanhado == true)
+          {
+            textoAdicional = "João está acompanhado!";
+          }
+          else
+          {
+            textoAdicional = "João não está acompanhado!";
+          }
+
+
+          if (idadeJoão >= 18 || acompanhado)
+          {
+            Console.WriteLine(textoAdicional);
+            Console.WriteLine("Pode entrar!");
+          }
+          else
+          {
+              Console.WriteLine("Não pode entrar!");
+          }
+
+          Console.WriteLine("Tecle enter para fechar ...");      
+          Console.ReadLine();    
+        }
+    }
+}
