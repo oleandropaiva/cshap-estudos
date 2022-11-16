e501174b3d2e64a775e424c9407473e9923e72f0
Leandro Paiva <leandroapaiva@yahoo.com>
2022-11-15 21:49:29 -0300

    cria calculaPoupança2

---
+++ calculaPoupanca/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {            
+        for(int contador = 0; contador <= 10; contador++)
+        {
+            Console.WriteLine(contador);
+        }
+        Console.ReadLine();
+    }
+}
