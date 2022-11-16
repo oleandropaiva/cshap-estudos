ae8c36873d906e7cf50eb13cc5ec02866b9e0003
Leandro Paiva <leandroapaiva@yahoo.com>
2022-11-15 22:39:57 -0300

    exec

--- calculaPoupanca2/Program.cs
+++ calculaPoupanca2/Program.cs
@@ -11,13 +11,15 @@ namespace MyNamespace
             double valorInvestido = 1000;
             // rendimento de 0.5 (0.005) ao mês
 
-            // mês 1
-            valorInvestido  =  valorInvestido  + valorInvestido *  0.005;
-            // mês 2
-            valorInvestido  =  valorInvestido  + valorInvestido *  0.005;
-            // mês 3
-            valorInvestido  =  valorInvestido  + valorInvestido *  0.005;
-            Console.WriteLine("Após um mês, você terá " + valorInvestido);
+            int mes = 1;
+
+            while (mes <= 12)
+            {
+              valorInvestido  =  valorInvestido  + valorInvestido *  0.005;
+              Console.WriteLine("Após um mês, você terá " + valorInvestido);
+
+              mes = mes + 1;
+            }
 
 
             Console.WriteLine("Tecle enter para fechar ...");
