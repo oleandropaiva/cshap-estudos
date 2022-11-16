6486a391687778e44e61aa3ecb8bcdbd3b02d5d2
Leandro Paiva <leandroapaiva@yahoo.com>
2022-11-15 22:12:48 -0300

    add  calc 3 meses

--- calculaPoupanca2/Program.cs
+++ calculaPoupanca2/Program.cs
@@ -11,6 +11,11 @@ namespace MyNamespace
             double valorInvestido = 1000;
             // rendimento de 0.5 (0.005) ao mês
 
+            // mês 1
+            valorInvestido  =  valorInvestido  + valorInvestido *  0.005;
+            // mês 2
+            valorInvestido  =  valorInvestido  + valorInvestido *  0.005;
+            // mês 3
             valorInvestido  =  valorInvestido  + valorInvestido *  0.005;
             Console.WriteLine("Após um mês, você terá " + valorInvestido);
 
