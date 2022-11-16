1def9b5bf4b22dd841a12438db947937ce378476
Leandro Paiva <leandroapaiva@yahoo.com>
2022-11-15 22:06:11 -0300

    desenvolvimento

--- calculaPoupanca2/Program.cs
+++ calculaPoupanca2/Program.cs
@@ -1,17 +1,20 @@
 ﻿using System;
-
-namespace MyNamespace
-{
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Executando o Projeto Calcula Poupança");
 
-            
+            double valorInvestido = 1000;
+            // rendimento de 0.5 (0.005) ao mês
+
+            valorInvestido  =  valorInvestido  + valorInvestido *  0.005;
+            Console.WriteLine(valorInvestido);
+
+
             Console.WriteLine("Tecle enter para fechar ...");
             Console.ReadLine();
     }
   }
-}
+
 
